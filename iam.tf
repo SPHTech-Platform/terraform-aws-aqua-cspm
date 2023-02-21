39ac68332a30f5af53668c7d7b04373552622d61
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2023-02-21 10:16:34 +0800

    disable the iam-assuable-role and used resource to create it

--- iam.tf
+++ iam.tf
@@ -32,23 +32,44 @@ resource "aws_iam_policy" "aqua_cspm_supplemental" {
   policy      = data.aws_iam_policy_document.aqua_cspm_supplemental.json
 }
 
-module "aqua_cspm_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-  version = "~> 5.9.0"
+# module "aqua_cspm_role" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+#   version = "~> 5.9.0"
 
-  create_role = true
+#   create_role = true
 
-  role_name        = "${local.name_prefix}-role"
-  role_description = "Assumable Role of Aqua SaaS"
+#   role_name        = "${local.name_prefix}-role"
+#   role_description = "Assumable Role of Aqua SaaS"
 
-  custom_role_trust_policy = data.aws_iam_policy_document.aqua_cspm_custom_trust.json
+#   custom_role_trust_policy = data.aws_iam_policy_document.aqua_cspm_custom_trust.json
 
-  custom_role_policy_arns = [
-    "arn:aws:iam::aws:policy/SecurityAudit",
-    aws_iam_policy.aqua_cspm_supplemental.arn,
-  ]
+#   custom_role_policy_arns = [
+#     "arn:aws:iam::aws:policy/SecurityAudit",
+#     aws_iam_policy.aqua_cspm_supplemental.arn,
+#   ]
+
+#   depends_on = [
+#     aws_lambda_invocation.external_id,
+#   ]
+# }
+
+resource "aws_iam_role" "aqua_cspm" {
+  name        = "${local.name_prefix}-role"
+  description = "Assumable Role of Aqua SaaS"
+
+  path                 = "/"
+  max_session_duration = "3600"
+
+  assume_role_policy = data.aws_iam_policy_document.aqua_cspm_custom_trust.json
 
   depends_on = [
     aws_lambda_invocation.external_id,
   ]
+}
+
+resource "aws_iam_role_policy_attachment" "aqua_cspm" {
+  count = length(local.aqua_cspm_role_policy_arns)
+
+  policy_arn = element(local.aqua_cspm_role_policy_arns, count.index)
+  role       = aws_iam_role.aqua_cspm.name
 }
--- locals.tf
+++ locals.tf
@@ -5,4 +5,9 @@ locals {
 
   external_id = jsondecode(aws_lambda_invocation.external_id.result)["data"]
   public_ip   = "3.231.74.65/32"
+
+  aqua_cspm_role_policy_arns = [
+    "arn:aws:iam::aws:policy/SecurityAudit",
+    aws_iam_policy.aqua_cspm_supplemental.arn,
+  ]
 }
