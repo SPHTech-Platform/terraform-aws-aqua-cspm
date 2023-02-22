5161b7d641479e769b3fb411b31e75d2a30b1c20
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2023-02-22 12:50:34 +0800

    fix json output

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
 
   secret_name = "/aquacspm/secret-cspm"
 
-  external_id = jsondecode(data.aws_lambda_invocation.external_id.result)["data"]["ExternalId"]
+  external_id = jsondecode(data.aws_lambda_invocation.external_id.result)["ExternalId"]
   public_ip   = "3.231.74.65/32"
 
   aqua_cspm_role_policy_arns = [
