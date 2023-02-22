83c4c5e5e580a1ed61c58a5ae4dd23ca3b3a9f42
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2023-02-22 15:22:56 +0800

    adding asian region details

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
   secret_name = "/aquacspm/secret-cspm"
 
   external_id = jsondecode(data.aws_lambda_invocation.external_id.result)["ExternalId"]
-  public_ip   = "3.231.74.65/32"
+  public_ip   = "13.215.18.141/32"
 
   aqua_cspm_role_policy_arns = [
     "arn:aws:iam::aws:policy/SecurityAudit",
