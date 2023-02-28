316f1b13fb5750bcdeb657950b6fc346b2b3fa01
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2023-02-28 18:17:42 +0800

    update the local

--- locals.tf
+++ locals.tf
@@ -3,8 +3,7 @@ locals {
 
   secret_name = "/aquacspm/secret-cspm"
 
-  external_id = "ds683bv"
-  # external_id = local.lambda_extid["status"] == "FAILED" ? jsondecode(data.aws_lambda_invocation.external_id.result)["message"] : jsondecode(data.aws_lambda_invocation.external_id.result)["ExternalId"]
+  external_id = jsondecode(data.aws_lambda_invocation.external_id.result)["ExternalId"]
   # public_ip   = "13.215.18.141/32"
 
   aqua_cspm_role_policy_arns = [
