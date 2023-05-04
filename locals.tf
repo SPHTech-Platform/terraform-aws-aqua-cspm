5fbf10777703eb6b0e646cffe9e5b7719cc04a2a
“Subramanian <smoneyan@gmail.com>
2023-05-04 12:12:33 +0800

    Add validation for disabling

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
   secret_name = "/aquacspm/secret-cspm"
 
   external_id        = jsondecode(aws_lambda_invocation.external_id.result)["status"] == "FAILED" ? jsondecode(aws_lambda_invocation.external_id.result)["message"] : jsondecode(aws_lambda_invocation.external_id.result)["ExternalId"]
-  sechub_external_id = jsondecode(aws_lambda_invocation.sechub_integration_external_id[0].result)["status"] == "FAILED" ? jsondecode(aws_lambda_invocation.sechub_integration_external_id[0].result)["message"] : jsondecode(aws_lambda_invocation.sechub_integration_external_id[0].result)["ExternalId"]
+  sechub_external_id = try(jsondecode(aws_lambda_invocation.sechub_integration_external_id[0].result)["status"] == "FAILED" ? jsondecode(aws_lambda_invocation.sechub_integration_external_id[0].result)["message"] : jsondecode(aws_lambda_invocation.sechub_integration_external_id[0].result)["ExternalId"], null)
 
   aquasec_account_id = "057012691312"
   # public_ip   = "13.215.18.141/32"
